a0bfc275ca8d3efffb21aeda8c672dfc7caffd02
BeautyVika <vikachk54@gmail.com>
2022-10-25 15:19:36 +0300

    refactor homework6

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -32,7 +32,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
         onBlur,
         onEnter,
         spanProps,
-
+        id,
         ...restProps // все остальные пропсы попадут в объект restProps
     }
 ) => {
@@ -64,6 +64,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
         <>
             {editMode ? (
                 <SuperInputText
+                    id={id}
                     autoFocus={autoFocus || true}
                     onBlur={onBlurCallback}
                     onEnter={onEnterCallback}
