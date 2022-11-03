429f614093dd2e7640ce3b9abb588d405f6c0e54
BeautyVika <vikachk54@gmail.com>
2022-11-03 11:30:25 +0300

    finish homework 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,16 +4,17 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => {
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
+        case 'sort':
+            const names = state.map(el => {
+                return {...el}
+            })
+            return action.payload === 'up'
+                ? names.sort((a,b) => a.name > b.name ? 1 : -1)
+                : names.sort((a,b) => a.name < b.name ? 1 : -1)
+        case 'check':
+            return state.filter(el => el.age >= 18)
         default:
             return state
     }
